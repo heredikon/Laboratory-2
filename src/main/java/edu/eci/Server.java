7c2fccf03db1945dfe37a2a38a124bbd35e1235b
Fabian Camilo Bohorquez Heredia <heredia800@gmail.com>
2019-01-27 15:46:35 -0500

    ejercicio funciones trigonometricas

--- src/main/java/edu/eci/Server.java
+++ src/main/java/edu/eci/Server.java
@@ -5,36 +5,66 @@
  */
 package edu.eci;
 
- import java.net.*;
- import java.io.*;
- public class Server {
-  public static void main(String[] args) throws IOException {
-   ServerSocket serverSocket = null;
-   try {
-    serverSocket = new ServerSocket(35000);
-   } catch (IOException e) {
-    System.err.println("Could not listen on port: 35000.");
-    System.exit(1);
-   }
-   Socket clientSocket = null;
-   try {
-    clientSocket = serverSocket.accept();
-   } catch (IOException e) {
-    System.err.println("Accept failed.");
-    System.exit(1);
-   }
-   PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-   BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-   String inputLine, outputLine;
-   while ((inputLine = in .readLine()) != null) {
-    System.out.println("Mensaje:"+inputLine);
-    outputLine = "Respuesta " + Integer.parseInt(inputLine)*Integer.parseInt(inputLine);
-         
-    out.println(outputLine);
-    if (outputLine.equals("Respuesta Bye")) break;
-   }
-   out.close(); in .close();
-   clientSocket.close();
-   serverSocket.close();
-  }
- }
+import java.net.*;
+import java.io.*;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        int funtion = 2;//cos
+        double answer = 0.0;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Mensaje:" + inputLine);
+            if (inputLine.equals("fun:sin")) {
+                funtion = 1;//sin
+            } else if (inputLine.equals("fun:cos")) {
+                funtion = 2;//cos
+            } else if (inputLine.equals("fun:tan")) {
+                funtion = 3;//tan
+            } else {
+                System.out.println(funtion);
+                switch (funtion) {
+                    case 1:
+                        System.out.println("case sin");
+                        answer = Math.sin(Double.parseDouble(inputLine));
+                        break;
+                    case 2:
+                        System.out.println("case cos");
+                        answer = Math.cos(Double.parseDouble(inputLine));
+                        break;
+                    case 3:
+                        System.out.println("case tan");
+                        answer = Math.tan(Double.parseDouble(inputLine));
+                        break;
+                }
+            }
+            outputLine = "Respuesta " + answer;
+
+            out.println(outputLine);
+            if (outputLine.equals("Respuesta Bye")) {
+                break;
+            }
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
